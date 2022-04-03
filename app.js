c3ff06c1dc333c2a7c9b36ed388d94acb8a74b27
avinashluhana <aviluhana@gmail.com>
2022-04-03 17:49:42 +0500

    connected with mongodb

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const app = express();
+mongoose.connect("mongodb://localhost:27017/Product").then(()=>{
+    console.log("connected with mongodb")
+}).catch((err)=>{
+    console.log(err)
+
+})
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.json());
@@ -14,10 +20,10 @@ const productSchema = new mongoose.Schema({
 
 const products = new mongoose.model("product", productSchema);
 
-app.post("api/v1/product/new", async (req, res) => {
+app.post("/api/v1/product/new", async (req, res) => {
   const productData = new products.create(req.body);
 
-  res.statu(200).json({
+  res.status(200).json({
     success: true,
     product,
   });
