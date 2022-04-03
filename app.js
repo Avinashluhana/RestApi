49166015fc6e62702acd8cf34a6d6d29b4b4c683
avinashluhana <aviluhana@gmail.com>
2022-04-03 17:32:18 +0500

    added post method

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const bodyParser = require("body-parser");
 const app = express();
 
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.json());
+
 const productSchema = new mongoose.Schema({
-    name:String,
-    description:String,
-    price:Number,
-
-})
-
+  name: String,
+  description: String,
+  price: Number,
+});
 
 const products = new mongoose.model("product", productSchema);
 
+app.post("api/v1/product/new", async (req, res) => {
+  const productData = new products.create(req.body);
 
-app.post()
+  res.statu(200).json({
+    success: true,
+    product,
+  });
+});
 
-
-
-app.listen(4000,()=>{
-    console.log("server is running");
-})
-
-
+app.listen(4000, () => {
+  console.log("server is running");
+});
