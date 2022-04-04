dd6b2aadd3525e8f5534e7acd2e6281021b827b4
avinashluhana <aviluhana@gmail.com>
2022-04-04 16:36:50 +0500

    fixed error

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const app = express();
-mongoose.connect("mongodb://localhost:27017/Product").then(()=>{
-    console.log("connected with mongodb")
-}).catch((err)=>{
-    console.log(err)
+mongoose
+  .connect("mongodb://localhost:27017")
+  .then(() => {
+    console.log("connected with mongodb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-})
-
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
 const productSchema = new mongoose.Schema({
@@ -21,11 +23,11 @@ const productSchema = new mongoose.Schema({
 const products = new mongoose.model("product", productSchema);
 
 app.post("/api/v1/product/new", async (req, res) => {
-  const productData = new products.create(req.body);
+  const productData = await products.create(req.body);
 
   res.status(200).json({
     success: true,
-    product,
+    productData,
   });
 });
 
